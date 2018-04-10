15c7853350d75a0863a26549b36817ed0066c9c4
Thomas Gies <thomas.gies@gmx.de>
2018-04-10 15:46:48 +0200

    ESLint added

--- app.js
+++ app.js
@@ -12,42 +12,45 @@ var ObjectId = require('mongodb').ObjectID;
 var cookieSession = require('cookie-session');
 var lessMiddleware = require('less-middleware');
 
-const url = 'mongodb://localhost:27017/';
+var url = 'mongodb://localhost:27017/';
 
 var app = express();
 
 app.use(function (req, res, next) {
-   var form = new formidable.IncomingForm({
-      encoding: 'utf-8',
-      uploadDir:  path.join(__dirname, 'uploads'),
-      multiples: true,
-      keepExtensions: true
-   })
-   form.once('error', console.log);
-   form.parse(req, function (err, fields, files) {
-      console.log("fields: " + fields);
-      console.log("files: " + files);
-      req.body = fields;
-      req.files = files;
-      next();
-   })
-})
-
-var db;
-MongoClient.connect(url, function(err, client) {
-   if (err) return console.error(err);
-   db = client.db("myproject");
+  var form = new formidable.IncomingForm({
+    encoding: 'utf-8',
+    uploadDir: path.join(__dirname, 'uploads'),
+    multiples: true,
+    keepExtensions: true
+  });
+  form.once('error', console.log);
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      console.log('ljljlj');
+    }
+    console.log('fields: ' + fields);
+    console.log('files: ' + files);
+    req.body = fields;
+    req.files = files;
+    next();
+  });
 });
 
-app.use(function(req, res, next) {
-   req.db = db;
-   req.ObjectId = ObjectId;
-   next();
+var db;
+MongoClient.connect(url, function (err, client) {
+  if (err) return console.error(err);
+  db = client.db('myproject');
+});
+
+app.use(function (req, res, next) {
+  req.db = db;
+  req.ObjectId = ObjectId;
+  next();
 });
 
 app.use(cookieSession({
-     name: 'session',
-     secret: 'Quaiqu4Un2cied8Haeh8hohge',
+  name: 'session',
+  secret: 'Quaiqu4Un2cied8Haeh8hohge'
 }));
 
 // view engine setup
@@ -58,38 +61,37 @@ app.locals.basedir = __dirname;
 app.use(logger('dev'));
 app.use(cookieParser());
 
-app.use(lessMiddleware(path.join(__dirname,'/public')));
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(lessMiddleware(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(function(req, res, next) {
-   if (req.path === '/login/login') {
-      next();
-   } else if (typeof(req.session.user)=='undefined') {
-      res.redirect('/login/login');
-   } else {
-      next();
-   }
+app.use(function (req, res, next) {
+  if (req.path === '/login/login') {
+    next();
+  } else if (typeof (req.session.user) === 'undefined') {
+    res.redirect('/login/login');
+  } else {
+    next();
+  }
 });
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-   next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-   // set locals, only providing error in development
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   // render the error page
-   res.status(err.status || 500);
-   res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
